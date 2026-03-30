fa38b9f8d3a67f7810a49fc64f0864208158bdf5
luizcarlosdvj-ops <luizcarlosdvj@gmail.com>
2026-03-30 14:46:10 -0300

    Fix marker duplication and add toggle functionality
    Removed duplicate marker addition for collection points and added toggle functionality for PEV elements.

--- script.js
+++ script.js
@@ -6,40 +6,40 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 // PONTOS DE COLETA
 const pontos = [
-   {
-  nome: "PEV Portal Mantiqueira",
-  coords: [-23.02783807934763, -45.59524964394488]
-},
   {
-  nome: "PEV Cecap ",
-  coords: [-23.038419303363188, -45.61880788931474]
+    nome: "PEV Portal Mantiqueira",
+    coords: [-23.02783807934763, -45.59524964394488]
+  },
+  {
+    nome: "PEV Cecap",
+    coords: [-23.038419303363188, -45.61880788931474]
   },
   { 
-  nome: "PEV Centro",
-  coords: [-23.028803162170558, -45.55452414441141]
-},
-{
-  nome: "PEV Imaculada",
-  coords: [-23.047449023860835, -45.545293672440415]
-},
-{
-  nome: "PEV Parque Três Marias II",
-  coords: [-23.031388956010513, -45.53782141016309]
-},
-{
-  nome: "PEV itaim",
-  coords: [-23.02017202574099, -45.52601969038315]
-},
-{
-  nome: "PEV Parque São Luiz",
-  coords: [-22.99772208852909, -45.560003250506306]
-},
-{
-  nome: "PEV Parque Piratininga",
-  coords: [-23.010131076369166, -45.593815713260426]
-},
+    nome: "PEV Centro",
+    coords: [-23.028803162170558, -45.55452414441141]
+  },
+  {
+    nome: "PEV Imaculada",
+    coords: [-23.047449023860835, -45.545293672440415]
+  },
+  {
+    nome: "PEV Parque Três Marias II",
+    coords: [-23.031388956010513, -45.53782141016309]
+  },
+  {
+    nome: "PEV Itaim",
+    coords: [-23.02017202574099, -45.52601969038315]
+  },
+  {
+    nome: "PEV Parque São Luiz",
+    coords: [-22.99772208852909, -45.560003250506306]
+  },
+  {
+    nome: "PEV Parque Piratininga",
+    coords: [-23.010131076369166, -45.593815713260426]
+  }
 ];
-// adicionar marcadores
+// adicionar marcadores (CORRIGIDO - sem duplicar)
 pontos.forEach(ponto => {
   L.marker(ponto.coords)
     .addTo(map)
@@ -48,12 +48,7 @@ pontos.forEach(ponto => {
       ♻️ Ponto de entrega voluntária
     `);
 });
-// MARCADORES
-pontos.forEach(ponto => {
-  L.marker(ponto.coords)
-    .addTo(map)
-    .bindPopup(`<b>${ponto.nome}</b>`);
-});
+
 // LOCALIZAÇÃO DO USUÁRIO
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(pos => {
@@ -64,4 +59,9 @@ if (navigator.geolocation) {
       .openPopup();
     map.setView(userCoords, 14);
   });
+}
+// TOGGLE DOS PEV
+function toggle(element) {
+  const pev = element.parentElement;
+  pev.classList.toggle("active");
 }
